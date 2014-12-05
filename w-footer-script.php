1604dbf3adaf9e1d4590f47cc314fbe71a55e7bd
Marco Lopez <mlopez@marostdevelopers.com>
2014-12-05 14:25:12 -0500

    eliminacion de enlace a scripts a no utilizar

--- w-footer-script.php
+++ w-footer-script.php
@@ -3,8 +3,5 @@
 <script src="assets/js/bootstrap.min.js"></script>
 <script src="assets/js/bootstrap-datepicker.js"></script>
 <script src="assets/js/jquery.sidr.min.js"></script>
-<script src="assets/js/jquery.carouFredSel-6.2.1-packed.js"></script>
-<script src="assets/js/jquery.touchSwipe.min.js"></script>
-<script src="assets/js/jquery.photobox.js"></script>
 <script src="assets/js/functions.js"></script>
 <!-- end:scripts -->
