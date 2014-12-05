b6f2fcae2e1888254907948210ed9a43f7fb5cb1
Marco Lopez <mlopez@marostdevelopers.com>
2014-12-05 14:23:06 -0500

    agregando paginas de galeria de fotos

---
+++ galeria-fotos.php
@@ -0,0 +1,141 @@
+<?php
+require_once("panel@viat/conexion/conexion.php");
+require_once("panel@viat/conexion/funciones.php");
+
+//VARIABLES DE URL
+$Req_Id=$_REQUEST["id"];
+$Req_Url=$_REQUEST["url"];
+
+##################################################################################################################
+//NOTICIA
+$rst_noticia=mysql_query("SELECT * FROM vtr_galeria WHERE id=$Req_Id AND publicar=1 AND fecha_publicacion<='$fechaActual';", $conexion);
+$fila_noticia=mysql_fetch_array($rst_noticia);
+
+//VARIABLES DE NOTICIA
+$Noticia_id=$fila_noticia["id"];
+$Noticia_url=$fila_noticia["url"];
+$Noticia_titulo=$fila_noticia["titulo"];
+$Noticia_contenido=$fila_noticia["contenido"];
+$Noticia_fechaPub=$fila_noticia["fecha_publicacion"];
+
+##################################################################################################################
+//NOTICIA - GALERIA DE FOTOS
+$rst_notFotos=mysql_query("SELECT * FROM vtr_galeria_slide WHERE noticia=$Noticia_id ORDER BY orden DESC", $conexion);
+
+##################################################################################################################
+//URLS
+$Noticia_UrlWeb=$web."noticia/".$Req_Id."-".$Req_Url;
+
+?>
+<!DOCTYPE html >
+<!--[if lt IE 7]> <html class="no-js ie6 oldie" lang="es"> <![endif]-->
+<!--[if IE 7]>    <html class="no-js ie7 oldie" lang="es"> <![endif]-->
+<!--[if IE 8]>    <html class="no-js ie8 oldie" lang="es"> <![endif]-->
+<!--[if IE 9]>    <html class="no-js ie9 oldie" lang="es"> <![endif]-->
+<!--[if gt IE 8]><!--> <html class="no-js" lang="es-ES"> <!--<![endif]-->
+<head>
+    <!-- start:global -->
+    <meta charset="utf-8">
+    <!-- end:global -->
+
+    <!-- start:page title -->
+    <title><?php echo $Noticia_titulo; ?> | Vialidad y Transporte Latinoamericano</title>
+    <!-- end:page title -->
+    
+    <!-- start:meta info -->
+    <meta name="keywords" content="" />
+    <meta name="description" content="">
+    <!-- end:meta info -->
+
+    <?php require_once('w-header-script.php') ?>
+
+    <!-- LIGHTBOX -->
+    <link rel="stylesheet" href="/libs/lightbox/css/lightbox.css">
+
+</head>
+<body>
+    
+    <!-- start:page outer wrap -->
+    <div id="page-outer-wrap">
+        <!-- start:page inner wrap -->
+        <div id="page-inner-wrap">    
+            
+            <?php require_once('w-header.php') ?>
+            
+            <!-- start:container -->
+            <div class="container">
+                
+                <!-- start:page content -->
+                <div id="page-content" class="clearfix">
+                    
+                    <!-- start:main -->
+                    <div id="main" class="article">
+                        
+                        <!-- start:article -->
+                        <article>
+                            
+                            <header>
+                                <h2><a href="#"><?php echo $Noticia_titulo; ?></a></h2>
+                                <span class="borderline"></span>
+                            </header>
+                            
+                            <!-- start:gallery section -->
+                            <section>
+                                
+                                <div id="article-gallery" class="row">
+
+                                    <?php while($fila_notFotos=mysql_fetch_array($rst_notFotos)){
+                                            $noticia_imagen=$fila_notFotos["imagen"];
+                                            $noticia_imagen_carpeta=$fila_notFotos["imagen_carpeta"];
+
+                                            $Noticia_UrlImg=$web."imagenes/galeria/".$noticia_imagen_carpeta."".$noticia_imagen;
+                                            $Noticia_UrlImgTh=$web."imagenes/galeria/".$noticia_imagen_carpeta."thumb/".$noticia_imagen;
+                                    ?>
+                                    
+                                    <div class="col-xs-6 col-sm-3">
+                                        <article class="clearfix">
+                                            <a href="<?php echo $Noticia_UrlImg; ?>" data-lightbox="galeria">
+                                                <img src="<?php echo $Noticia_UrlImgTh; ?>" width="300" height="200" alt="<?php echo $Noticia_titulo; ?>" class="img-responsive" />
+                                                <div class="zoomix"><i class="fa fa-search"></i></div>
+                                            </a>
+                                        </article>
+                                    </div>
+
+                                    <?php } ?>
+                                    
+                                    
+                                </div>                                
+                                
+                            </section>
+                            <!-- end:gallery section -->
+
+                        </article>
+                        <!-- end:article -->
+                        
+                    </div>
+                    <!-- end:main -->
+                    
+                    
+                    <?php require_once('w-sidebar.php') ?>     
+                                  
+                </div>
+                <!-- end:page content -->
+            
+            </div>
+            <!-- end:container -->    
+
+            <?php require_once('w-footer.php') ?>
+        
+        </div>
+        <!-- end:page inner wrap -->
+    </div>
+    <!-- end:page outer wrap -->
+    
+    <?php require_once('w-footer-script.php') ?>
+
+    <!-- LIGHTBOX -->
+    <script src="/libs/lightbox/js/jquery-1.11.0.min.js"></script>
+    <script src="/libs/lightbox/js/lightbox.js"></script>
+    
+</body>
+</html>
---
+++ galeria.php
@@ -0,0 +1,142 @@
+<?php
+require_once('panel@viat/conexion/conexion.php');
+require_once("panel@viat/conexion/funciones.php");
+
+//VARIABLES
+$Req_UrlWeb=$web."galerias";
+
+################################################################
+//PAGINACION DE NOTICIAS
+require("libs/pagination/class_pagination.php");
+
+//INICIO DE PAGINACION
+$page           = (isset($_GET['page'])) ? intval($_GET['page']) : 1;
+$rst_noticias   = mysql_query("SELECT COUNT(*) as count FROM vtr_galeria WHERE fecha_publicacion<='$fechaActual' ORDER BY fecha_publicacion DESC;", $conexion);
+$row            = mysql_fetch_assoc($rst_noticias);
+$generated      = intval($row['count']);
+$pagination     = new Pagination("8", $generated, $page, $Req_UrlWeb."&page", 1, 0);
+$start          = $pagination->prePagination();
+$rst_noticias   = mysql_query("SELECT * FROM vtr_galeria WHERE fecha_publicacion<='$fechaActual' ORDER BY fecha_publicacion DESC LIMIT $start, 8", $conexion);
+
+?>
+<!DOCTYPE html >
+<!--[if lt IE 7]> <html class="no-js ie6 oldie" lang="es"> <![endif]-->
+<!--[if IE 7]>    <html class="no-js ie7 oldie" lang="es"> <![endif]-->
+<!--[if IE 8]>    <html class="no-js ie8 oldie" lang="es"> <![endif]-->
+<!--[if IE 9]>    <html class="no-js ie9 oldie" lang="es"> <![endif]-->
+<!--[if gt IE 8]><!--> <html class="no-js" lang="es-ES"> <!--<![endif]-->
+<head>
+    <!-- start:global -->
+    <meta charset="utf-8">
+    <!-- end:global -->
+
+    <!-- start:page title -->
+    <title>Vialidad y Transporte Latinoamericano</title>
+    <!-- end:page title -->
+    
+    <!-- start:meta info -->
+    <meta name="keywords" content="" />
+    <meta name="description" content="">
+    <!-- end:meta info -->
+
+    <?php require_once('w-header-script.php') ?>
+
+</head>
+<body>
+    
+    <!-- start:page outer wrap -->
+    <div id="page-outer-wrap">
+        <!-- start:page inner wrap -->
+        <div id="page-inner-wrap">    
+            
+            <?php require_once('w-header.php') ?>
+            
+            <!-- start:container -->
+            <div class="container">
+                
+                <!-- start:page content -->
+                <div id="page-content" class="clearfix">
+                    
+                    <!-- start:main -->
+                    <div id="main">
+                        
+                        <!-- start:row -->
+                        <div class="row">
+                            
+                            <!-- start:col -->
+                            <div class="col-md-12">
+
+                                <header>
+                                    <h2><a href="#">Galería de Fotos</a></h2>
+                                    <span class="borderline"></span>
+                                </header>
+                                
+                                <!-- start:row -->
+                                <div class="row">
+
+                                    <?php while($fila_noticias=mysql_fetch_array($rst_noticias)){
+                                        $noticia_id=$fila_noticias["id"];
+                                        $noticia_url=$fila_noticias["url"];
+                                        $noticia_titulo=stripslashes($fila_noticias["titulo"]);
+                                        $noticia_fechaPub=$fila_noticias["fecha_publicacion"];
+
+                                        //FOTOS
+                                        $rst_fotos=mysql_query("SELECT * FROM vtr_galeria_slide WHERE noticia=$noticia_id AND orden=0", $conexion);
+                                        $fila_fotos=mysql_fetch_array($rst_fotos);
+
+                                        $noticia_imagen=$fila_fotos["imagen"];
+                                        $noticia_imagen_carpeta=$fila_fotos["imagen_carpeta"];
+
+                                        //URLS
+                                        $noticia_UrlWeb=$web."galeria/".$noticia_id."-".$noticia_url;
+                                        $noticia_UrlImg=$web."imagenes/galeria/".$noticia_imagen_carpeta."thumb/".$noticia_imagen;
+                                    ?>
+                                    
+                                    <!-- start:col -->
+                                    <div class="col-sm-6">
+                                        <!-- start:article.linkbox -->
+                                        <article class="linkbox cat-showtime">
+                                            <a href="<?php echo $noticia_UrlWeb; ?>">
+                                                <img src="<?php echo $noticia_UrlImg; ?>" width="265" height="160" alt="<?php echo $noticia_titulo; ?>" class="img-responsive" />
+                                                <div class="overlay">
+                                                    <h3><?php echo $noticia_titulo; ?></h3>
+                                                </div>
+                                            </a>
+                                        </article>
+                                        <!-- end:article.linkbox -->
+                                    </div>
+                                    <!-- end:col -->
+
+                                    <?php } ?>
+
+                                </div>
+                                <!-- end:row -->
+                                
+                            </div>
+                            <!-- end:col -->
+                            
+                        </div>
+                        <!-- end:row -->        
+                        
+                    </div>
+                    <!-- end:main -->
+                    
+                    <?php require_once('w-sidebar.php') ?>
+
+                </div>
+                <!-- end:page content -->
+            
+            </div>
+            <!-- end:container -->    
+
+            <?php require_once('w-footer.php') ?>
+        
+        </div>
+        <!-- end:page inner wrap -->
+    </div>
+    <!-- end:page outer wrap -->
+    
+    <?php require_once('w-footer-script.php') ?>
+    
+</body>
+</html>
