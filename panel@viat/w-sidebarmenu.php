2c2edec032dd96299c577169f886dde90411b707
Marco Lopez <mlopez@marostdevelopers.com>
2014-12-17 11:56:01 -0500

    enlace a seccion paginas desde administrador

--- panel@viat/w-sidebarmenu.php
+++ panel@viat/w-sidebarmenu.php
@@ -10,6 +10,7 @@
         <li><a href="<?php echo $url_admin; ?>paginas/noticias/lista.php" title=""><span class="icos-frames"></span>Noticias</a></li>
         <li><a href="<?php echo $url_admin; ?>paginas/noticias-tag/lista.php" title=""><span class="icos-frames"></span>Noticias - Etiquetas</a></li>
         <li><a href="<?php echo $url_admin; ?>paginas/noticias-seccion/lista.php" title=""><span class="icos-frames"></span>Noticias - Secciones</a></li>
+        <li><a href="<?php echo $url_admin; ?>paginas/paginas/lista.php" title=""><span class="icos-frames"></span>Páginas</a></li>
         <li><a href="<?php echo $url_admin; ?>paginas/portada-es/lista.php" title=""><span class="icos-frames"></span>Edición Impresa</a></li>
         <li><a href="<?php echo $url_admin; ?>paginas/usuarios/lista.php" title=""><span class="icos-frames"></span>Usuarios</a></li>
     </ul>
