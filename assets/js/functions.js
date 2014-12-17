a2c2b57e7fbbacd4410c69c9bcb0701bcbed2d96
Marco Lopez <mlopez@marostdevelopers.com>
2014-12-17 11:16:05 -0500

    eliminacion de scripts a no utilizar

--- assets/js/functions.js
+++ assets/js/functions.js
@@ -16,32 +16,7 @@
         $(this).parent().find('li').removeClass('current');
         $(this).addClass('current');
     });
-    
-    // Homepage slider
-    $('#slider-carousel').carouFredSel({
-        width: '100%',
-        height: 'auto',
-        prev: '#slider-prev',
-        next: '#slider-next',
-        auto: false,
-        responsive: true,
-        transition: true,
-        swipe: {
-                onMouse: true,
-                onTouch: true
-        },
-        scroll : {
-            items           : 1,
-            easing          : "quadratic",
-            duration        : 1000,                         
-            pauseOnHover    : true
-        }      
-    });
-    
-    // Init photobox
-    $('#weekly-gallery').photobox('a',{ time:0 });
-    $('#article-gallery').photobox('a',{ time:0 });
-    
+        
     // Init datepicker for archive page
     $('#archive-date-picker').datepicker({
         format: 'mm/dd/yyyy'
@@ -51,18 +26,6 @@
     $('.scrollToTop').click(function(){
         $('html, body').animate({scrollTop : 0},800);
         return false;
-    });
-    
-    //Contact form
-    var contactForm = $('#contactForm');
-    var successForm = $('.alert-success', contactForm);
-    var errorForm = $('.alert-danger', contactForm);
-    
-    $("#contactForm").validate({
-        submitHandler: function (form) {
-            successForm.removeClass('hide').show();
-            errorForm.hide();            
-        }
     });
     
 })(jQuery);
