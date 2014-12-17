2201e961f48deaae391c1bb8b391a6c26a19b87b
Marco Lopez <mlopez@marostdevelopers.com>
2014-12-17 11:14:26 -0500

    eliminacion de secciones a no utilizar

--- panel@viat/paginas/galeria-slider/s-ordenar.php
+++
@@ -1,9 +0,0 @@
-<?php
-session_start();
-include("../../conexion/conexion.php");
-
-foreach ($_GET['listItem'] as $position => $item):
-	$sql[] = mysql_query("UPDATE ".$tabla_suf."_galeria_slide SET orden=$position WHERE id=$item", $conexion);
-endforeach;
-
-?>
