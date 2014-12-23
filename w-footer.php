5b0a263879fc647e33edb24407dc8cffe87f55dc
Marco Lopez <mlopez@marostdevelopers.com>
2014-12-23 11:43:02 -0500

    cambio de texto en footer y agregando opcion entrevista en menu inferior

--- w-footer.php
+++ w-footer.php
@@ -12,6 +12,7 @@
                 <li class="cat-lifestyle"><a href="categoria/3/tecnologia">Tecnología</a></li>
                 <li class="cat-showtime"><a href="historia/">Historia</a></li>
                 <li class="cat-tech"><a href="eventos">Eventos</a></li>
+                <li class="cat-tech"><a href="entrevista">Entrevista</a></li>
             </ul>                            
         </nav>
         
@@ -25,14 +26,12 @@
             <!-- end:col -->
             
             <!-- start:col -->
-            <div class="col-sm-6 col-md-5">
+            <div class="col-sm-12 col-md-9">
                 <h4>Sobre <?php echo $web_nombre; ?></h4>
-                <p>Aliquam erat volutpat. Aliquam porta nibh id odio ornare ullamcorper. Etiam sit amet consectetur arcu. Suspendisse dolor nisl, eleifend quis metus a, sollicitudin semper orci.</p>
+                <p>La revista VIALIDAD Y TRANSPORTE LATINOAMERICANO es una publicación que surge con el objetivo de canalizar el intercambio de experiencias e informaciones y servir como una tribuna de debate alturado y técnico en torno a las perspectivas y posibilidades sobre el tema de vialidad y transportes.</p>
                 <a href="#"><i class="fa fa-twitter fa-lg"></i></a>
                 <a href="#"><i class="fa fa-facebook-square fa-lg"></i></a>
                 <a href="#"><i class="fa fa-linkedin-square fa-lg"></i></a>
-                <a href="#"><i class="fa fa-google-plus-square fa-lg"></i></a>
-                <a href="#"><i class="fa fa-vimeo-square fa-lg"></i></a>
                 <a href="#"><i class="fa fa-youtube fa-lg"></i></a>
             </div>
             <!-- end:col -->
