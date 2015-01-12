8143aeb651df2432674d06548a2ba0e248133ae1
Marco Lopez <mlopez@marostdevelopers.com>
2015-01-12 17:51:30 -0500

    eliminando opciones Etiquetas y Secciones

--- panel@viat/w-sidebarmenu.php
+++ panel@viat/w-sidebarmenu.php
@@ -9,8 +9,6 @@
         <li><a href="<?php echo $url_admin; ?>paginas/empresa/f-editar.php?id=1" title=""><span class="icos-frames"></span>Empresa</a></li>
         <li><a href="<?php echo $url_admin; ?>paginas/eventos/lista.php" title=""><span class="icos-frames"></span>Eventos</a></li>
         <li><a href="<?php echo $url_admin; ?>paginas/noticias/lista.php" title=""><span class="icos-frames"></span>Noticias</a></li>
-        <li><a href="<?php echo $url_admin; ?>paginas/noticias-tag/lista.php" title=""><span class="icos-frames"></span>Noticias - Etiquetas</a></li>
-        <li><a href="<?php echo $url_admin; ?>paginas/noticias-seccion/lista.php" title=""><span class="icos-frames"></span>Noticias - Secciones</a></li>
         <li><a href="<?php echo $url_admin; ?>paginas/paginas/f-editar.php?id=1" title=""><span class="icos-frames"></span>Historia</a></li>
         <li><a href="<?php echo $url_admin; ?>paginas/portada/lista.php" title=""><span class="icos-frames"></span>Revista</a></li>
         <li><a href="<?php echo $url_admin; ?>paginas/usuarios/lista.php" title=""><span class="icos-frames"></span>Usuarios</a></li>
