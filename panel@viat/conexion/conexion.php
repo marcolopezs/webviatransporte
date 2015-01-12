32b48fb35f8abd4f1b7f9bc1106c8e3de992f189
Marco Lopez <mlopez@marostdevelopers.com>
2015-01-11 20:28:03 -0500

    agregando opciones globales de correo y social media

--- panel@viat/conexion/conexion.php
+++ panel@viat/conexion/conexion.php
@@ -23,6 +23,10 @@ global $usuario_apellido;
 global $usuario_email;
 global $web;
 global $web_nombre;
+global $web_correo;
+global $web_facebook;
+global $web_twitter;
+global $web_youtube;
 global $fechaActual;
 
 //VARIABLES
@@ -36,6 +40,10 @@ $rst_empresa=mysql_query("SELECT * FROM ".$tabla_suf."_empresa WHERE id=1;", $co
 $fila_empresa=mysql_fetch_array($rst_empresa);
 $web=$fila_empresa["web"];
 $web_nombre=$fila_empresa["nombre"];
+$web_correo=$fila_empresa["correo_contacto"];
+$web_facebook=$fila_empresa["facebook"];
+$web_twitter=$fila_empresa["twitter"];
+$web_youtube=$fila_empresa["youtube"];
 
 //URL DE ARCHIVOS
 $url_admin=$web."".$carpeta_admin."/";
