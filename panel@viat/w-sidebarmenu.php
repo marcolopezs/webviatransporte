3cb78b62daaed4aaf9446382f9c8b62f575a0ebf
Marco Lopez <mlopez@marostdevelopers.com>
2015-01-11 20:10:30 -0500

    agregando en menu opcion empresa e historia

--- panel@viat/w-sidebarmenu.php
+++ panel@viat/w-sidebarmenu.php
@@ -6,11 +6,12 @@
     
     <!-- Sidebar subnav -->
     <ul class="subNav">
+        <li><a href="<?php echo $url_admin; ?>paginas/empresa/f-editar.php?id=1" title=""><span class="icos-frames"></span>Empresa</a></li>
         <li><a href="<?php echo $url_admin; ?>paginas/eventos/lista.php" title=""><span class="icos-frames"></span>Eventos</a></li>
         <li><a href="<?php echo $url_admin; ?>paginas/noticias/lista.php" title=""><span class="icos-frames"></span>Noticias</a></li>
         <li><a href="<?php echo $url_admin; ?>paginas/noticias-tag/lista.php" title=""><span class="icos-frames"></span>Noticias - Etiquetas</a></li>
         <li><a href="<?php echo $url_admin; ?>paginas/noticias-seccion/lista.php" title=""><span class="icos-frames"></span>Noticias - Secciones</a></li>
-        <li><a href="<?php echo $url_admin; ?>paginas/paginas/lista.php" title=""><span class="icos-frames"></span>Páginas</a></li>
+        <li><a href="<?php echo $url_admin; ?>paginas/paginas/f-editar.php?id=1" title=""><span class="icos-frames"></span>Historia</a></li>
         <li><a href="<?php echo $url_admin; ?>paginas/portada/lista.php" title=""><span class="icos-frames"></span>Revista</a></li>
         <li><a href="<?php echo $url_admin; ?>paginas/usuarios/lista.php" title=""><span class="icos-frames"></span>Usuarios</a></li>
     </ul>
